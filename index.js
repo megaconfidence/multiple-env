8f5f88270d7c5fc1b6c3b84dc0222d6b77956361
Confidence Okoghenun <cokoghenun@gmail.com>
2022-07-12 17:58:06 +0100

    feat: Adds env switcher

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const bodyParser = require("body-parser");
+
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let activeEnv = '';
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.json({activeEnv})
 })
 
-app.listen(3000)
+app.post('/', function (req, res) {
+  activeEnv = req.body.env;
+  res.json({activeEnv})
+})
+
+const port = process.env.PORT || 80;
+
+app.listen(port, () => {
+  console.log('🚀 Server on port ' + port);
+});
