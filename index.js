9757745d9b8e75638a7a9731f8302ccd3d00885a
Confidence Okoghenun <cokoghenun@gmail.com>
2022-07-12 17:29:55 +0100

    feat: Adds vehicles

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const express = require('express')
-const app = express()
+const faker = require('faker');
+const express = require('express');
+
+const app = express();
+
+const vehicle = () => ({
+  name: faker.vehicle.vehicle(),
+  model: faker.vehicle.model(),
+  manufacturer: faker.vehicle.manufacturer(),
+  type: faker.vehicle.type(),
+  color: faker.vehicle.color(),
+  fuel: faker.vehicle.fuel(),
+});
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  let data = [];
+  for (let i = faker.datatype.number({ min: 20, max: 150 }); i > 0; i--) {
+    data.push(vehicle());
+  }
+  res.json(data);
+});
 
-app.listen(3000)
+const port = process.env.PORT || 80;
+
+app.listen(port, () => {
+  console.log('🚀 Server on port ' + port);
+});
