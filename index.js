7c839aab79dbf72c33c4baea264cc83f546b0363
Confidence Okoghenun <cokoghenun@gmail.com>
2022-07-12 17:26:02 +0100

    feat: Adds user

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+const faker = require('faker')
 const express = require('express')
+
 const app = express()
 
+const user = () => ({
+  name: faker.name.firstName(),
+  age: faker.datatype.number(120),
+  phone: faker.phone.phoneNumber(),
+  gender: faker.name.gender(),
+  jobType: faker.name.jobType(),
+  jobTitle: faker.name.jobTitle(),
+})
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  let data = [];
+  for(let i = faker.datatype.number({min: 20, max: 150}); i > 0; i--) {
+    data.push(user())
+  }
+  res.json(data)
 })
 
 app.listen(3000)
