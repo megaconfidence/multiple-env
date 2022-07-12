f645e28c141361ba07db72885b29d1c3902e478f
Confidence Okoghenun <cokoghenun@gmail.com>
2022-07-12 17:32:45 +0100

    feat: Adds company

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const express = require('express')
-const app = express()
+const faker = require('faker');
+const express = require('express');
 
+const app = express();
+
+const company = () => ({
+  name: faker.company.companyName(),
+  location: faker.address.country(),
+  size: faker.datatype.number(90000),
+  tagline: faker.company.catchPhrase(),
+  product: faker.commerce.productName(),
+  productDescriptoin: faker.commerce.productDescription(),
+});
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  let data = [];
+  for (let i = faker.datatype.number({ min: 20, max: 150 }); i > 0; i--) {
+    data.push(company());
+  }
+  res.json(data);
+});
 
-app.listen(3000)
+const port = process.env.PORT || 80;
+
+app.listen(port, () => {
+  console.log('🚀 Server on port ' + port);
+});
